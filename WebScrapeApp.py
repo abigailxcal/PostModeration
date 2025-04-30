db4a30cabe994f91e5f83651707c4bd2c11b6567
rane20 <145800950+rane20@users.noreply.github.com>
2025-04-29 23:16:53 -0700

    updated with balanced dataset

--- WebScrapeApp.py
+++ WebScrapeApp.py
@@ -24,8 +24,8 @@ st.markdown(
 # ===================== MODEL LOADING =====================
 @st.cache_resource
 def load_model():
-    model = DistilBertForSequenceClassification.from_pretrained("Bert_Model1")
-    tokenizer = DistilBertTokenizer.from_pretrained("Bert_Model1")
+    model = DistilBertForSequenceClassification.from_pretrained("Bert_Model2")
+    tokenizer = DistilBertTokenizer.from_pretrained("Bert_Model2")
     model.eval()
     return model, tokenizer
 
